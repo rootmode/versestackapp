b149d8d61725d315eb80756d0c4d1dd3793a480e
RootMode <rootmod3@protonmail.com>
2025-12-19 10:47:24 -0800

    merged book into main updated user pojo

--- app/src/main/java/com/example/versestackapp/database/LibraryDatabase.java
+++ app/src/main/java/com/example/versestackapp/database/LibraryDatabase.java
@@ -7,11 +7,9 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
-
 import com.example.versestackapp.Book;
 import com.example.versestackapp.Borrow;
 import com.example.versestackapp.User;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
