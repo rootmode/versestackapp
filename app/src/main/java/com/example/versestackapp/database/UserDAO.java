c03f35304d0e55d305644e8925ea90d5c733961a
RootMode <rootmod3@protonmail.com>
2025-12-19 12:10:45 -0800

    user dao and activity xml

--- app/src/main/java/com/example/versestackapp/database/UserDAO.java
+++ app/src/main/java/com/example/versestackapp/database/UserDAO.java
@@ -1,9 +1,9 @@
 package com.example.versestackapp.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
-
 import com.example.versestackapp.User;
 
 @Dao
@@ -11,9 +11,12 @@ public interface UserDAO {
     @Insert
     void insert(User user);
 
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    LiveData<User> getUserByUsername(String username);
+
+    @Query("SELECT * FROM users WHERE userId = :userId LIMIT 1")
+    LiveData<User> getUserByUserId(int userId);
+
     @Query("DELETE FROM users")
     void deleteAll();
-
-    @Query("SELECT userId FROM users WHERE libraryCardId = :cardId")
-    Integer getUserIdByCardId(int cardId);
 }
